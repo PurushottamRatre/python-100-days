1f1ad8bd85562f6d4d17015856a876b43822db59
PurushottamRatre <ratrepurushottam@gmail.com>
2024-01-07 14:31:58 +0530

    inital project commit

---
+++ Excercise 9  Treasure Map/main.py
@@ -0,0 +1,14 @@
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input("Where do you want to put the treasure? ") # a1,b1,c1,a2,b2,c2,a3,b3,c3
+
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
+
+print(f"{line1}\n{line2}\n{line3}")
---
+++ excercise 1 life in weeks/main.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+# we are taking 90 years as ideal age
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left.")
